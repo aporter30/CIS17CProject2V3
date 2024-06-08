a308553779945aa7270b8c27d9916f26da9bff9d
Alejandro Porter <107812659+aporter30@users.noreply.github.com>
2024-06-07 23:43:02 -0700

    Create card.h

---
+++ card.h
@@ -0,0 +1,20 @@
+/* 
+ * File:   card.h
+ * Author: ajporter
+ *
+ * Created on May 28, 2024, 3:12 PM
+ */
+
+#ifndef CARD_H
+#define CARD_H
+
+#include <string>
+using namespace std;
+
+struct Card {
+    string suit;
+    string rank;
+    int value;
+};
+
+#endif /* CARD_H */
